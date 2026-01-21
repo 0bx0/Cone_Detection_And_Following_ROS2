25cffdda6037e67efbeff50064251a0336318a0c
Vishesh Agrawal <167441615+Vishesh-Ag-BITSG@users.noreply.github.com>
2026-01-21 05:33:49 +0530

    Include model files in setup.py data files
    Added model files to the package data files in setup.py.

--- cone_detector/setup.py
+++ cone_detector/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'cone_detector'
 
@@ -7,8 +8,13 @@ setup(
     version='0.0.1',
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        # Package index
+        ('share/ament_index/resource_index/packages',
+         ['resource/' + package_name]),
+        ('share/' + package_name,
+         ['package.xml']),
+        ('share/' + package_name + '/models',
+         glob('models/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
